2130a754031a2d89ba8423cdc9eb24c3426f32a6
Vani2409 <jv.gdv24@gmail.com>
2023-06-01 22:47:41 +0530

    shellscript

---
+++ shellscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This is a simple script for beginners
+
+# Prompt the user for their name
+echo "Hello! What is your name?"
+
+# Read the user's input
+read name
+
+# Greet the user
+echo "Nice to meet you, $name!"
+echo "This is a simple shell script for beginners."
+
+# Display the current date and time
+echo "The current date and time is: $(date)"
+
+# Display a list of files in the current directory
+echo "Here are the files in the current directory:"
+ls
